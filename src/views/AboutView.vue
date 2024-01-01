614b86026fd9cc23c17df4f27da4247dc1194be1
engsiam <siam.softwareengineer@gmail.com>
2024-01-01 16:04:54 +0600

    add

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1 class="text-3xl bg-pink-500">This is an about page</h1>
+  </div>
+</template>
+
+
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+   <h1 class="bg-red-700 text-2xl">home</h1>
+  </main>
+</template>
